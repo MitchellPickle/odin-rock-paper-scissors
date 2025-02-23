cb6b416af6801a3c513c3ee43eb0316acf3d08aa
MitchellPickle <94079796+MitchellPickle@users.noreply.github.com>
2025-02-23 05:12:33 +0000

    Modified RockPaperScissors.js with player choice functionality.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -2,34 +2,121 @@ console.log("Hello World!");
 
 function getComputerChoice()
 {
-    computerChoiceNum = Math.floor(Math.random() * 3) + 1;
+    let computerChoiceNum = Math.floor(Math.random() * 3) + 1;
     console.log(computerChoiceNum);
 
     let computerChoiceString;
 
     if(computerChoiceNum == 1)
     {
-        computerChoiceString = "Rock";
+        computerChoiceString = "rock";
+        console.log("Computer Choice: " + computerChoiceString);
+        return computerChoiceString;
     }
     else if(computerChoiceNum == 2)
     {
-        computerChoiceString = "Paper";
+        computerChoiceString = "paper";
+        console.log("Computer Choice: " + computerChoiceString);
+        return computerChoiceString;
+        
     }
     else if(computerChoiceNum == 3)
     {
-        computerChoiceString = "Scissors"
+        computerChoiceString = "scissors";
+        console.log("Computer Choice: " + computerChoiceString);
+        return computerChoiceString;
     }
     else
     {
         console.log("ERROR: Computer choice isn't Rock, Paper, or Scissors");
+        alert("ERROR: Computer choice isn't Rock, Paper, or Scissors");
     }
 
-    console.log("Computer Choice: " + computerChoiceString);
+    // console.log("Computer Choice: " + computerChoiceString);
 }
 
-function getHumanChoice()
+function getHumanChoice(computerChoice)
 {
+    let humanChoice = prompt("Enter your choice (Rock = rock or 1. Paper = paper or 2. Scissors = scissors or 3.) \nNOTE: Case Sensitive");
+    console.log("Human Choice: " + humanChoice);
+    let outcome;
+
+    //If the player chooses rock
+    if(humanChoice == 1 || humanChoice == "rock")
+    {
+        if(computerChoice == "rock")
+        {
+            outcome = "Tie!"
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "paper")
+        {
+            outcome = "You Lost!"
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "scissors")
+        {
+            outcome = "You Win!";
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+    }
+
+    //If the player chooses paper
+    else if(humanChoice == 2 || humanChoice == "paper")
+    {
+        if(computerChoice == "rock")
+        {
+            outcome = "You Win!"
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "paper")
+        {
+            outcome = "Tie!";
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "scissors")
+        {
+            outcome = "You Lost!";
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+    }
     
+    //If the player chooses scissors
+    else if(humanChoice == 3 || humanChoice == "scissors")
+    {
+        if(computerChoice == "rock")
+        {
+            outcome = "You Lost!";
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "paper")
+        {
+            outcome = "You Win!"
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+        if(computerChoice == "scissors")
+        {
+            outcome = "Tie!";
+            alert("Round Outcome: " + outcome);
+            return outcome;
+        }
+    }
+
+    //Returns alert if player choice isn't rock, paper, or scissors
+    else
+    {
+        alert("ERROR: Player Choice was not rock, paper, or scissors.");
+        getHumanChoice(computerChoice);
+    }
 }
 
-getComputerChoice();
+let computerChoice = getComputerChoice();
+getHumanChoice(computerChoice);
